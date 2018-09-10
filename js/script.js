e0aab54b7e243fe06e058a60595517f909f5a5cb
sivaraj <sivaraj.r93@gmail.com>
2018-09-11 00:45:29 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,16 +8,35 @@ jQuery(function($){
 	    __accordion__title = $('.accordion-title');
 	    __accodion__content = $('.accordion-content');
 	    __accodion__active = 'active';
-	    __accodion__active_added = "$('.active')";
+	    __accodion__active_added = $('.active');
+	    __click = 'click';
 	   
 	    __this = $(this);
 
 	    var settings = $.extend({
-            color:'#cff06f',
             alwaysOpen:true,
+            activePanel:'Null',
         }, option);
 
-	    __accordion__title.on('click',function(){
+        if(__accordion__title.hasClass(__accodion__active)) {
+        	__accodion__active_added.next().show();
+        }
+
+        __count = 1;	
+    	__accodion.each(function(){
+    		__this = $(this);
+    		__this.attr('id','data-id-'+__count);
+    		if(settings.activePanel == __count) {
+    			$('#data-id-'+settings.activePanel).siblings().find(__accordion__title).removeClass(__accodion__active);
+    			$('#data-id-'+settings.activePanel).siblings().find(__accodion__content).hide();
+    			$('#data-id-'+settings.activePanel).find(__accodion__content).show();
+    			$('#data-id-'+settings.activePanel).find(__accordion__title).addClass(__accodion__active);
+    		}
+    		__count ++;
+
+    	});
+        
+	    __accordion__title.on(__click,function(){
 	    	__this = $(this);
 
 	    	if(settings.alwaysOpen == true) {
@@ -25,16 +44,13 @@ jQuery(function($){
 	    		__this.next().slideDown();
 	    		__this.parent().siblings().find('.'+__accodion__active).removeClass(__accodion__active);
 	    		__this.addClass(__accodion__active);
-	    		__this.parent().siblings().find(__accordion__title).css('background-color','#ffffff');
-	    		__this.css('background-color',settings.color);
 	    	}else {
 	    		__this.parent().siblings().find(__accodion__content).slideUp();
 	    		__this.next().slideToggle();
+	    		__this.parent().siblings().find('.'+__accodion__active).removeClass(__accodion__active);
 	    		__this.toggleClass(__accodion__active);
-	    		__this.parent().siblings().find(__accordion__title).css('background-color','#ffffff');
-	    		__this.css('background-color',settings.color);
 	    	}
-
+	    	return false;
 	    })
 	};
 });
